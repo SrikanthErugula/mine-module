3760230951a863651056c519eaecce3e4f775e33
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-29 13:04:43 +0530

    terraform_start

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ resource "aws_vpc" "main" {
    enable_dns_hostnames = true # see below for deatiled
 
 tags =  merge(
+     var.vpc_tags, 
     local.common_tags, # MAP 1..# see below for deatils
-    var.vpc_tags, # map 1 # for user purpose ki 
+   # var.vpc_tags, # map 1 # for user purpose ki 
     # before {} enni vunna adhi map 1 kidha ki vastai
     {
         Name = local.common_name_suffix #  map 2
