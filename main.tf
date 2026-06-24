ce7e7bb4ea7d77786368fc3f701ad57040d0d3c0
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-06-24 16:09:30 +0530

    modified_code

--- main.tf
+++ main.tf
@@ -18,10 +18,19 @@
 
 # practice 24/06/27
 
-resource "aws_insta" "this" {
+# resource "aws_instance" "this" {
+#   ami = var.ami_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = var.sg_ids
+#   tags = var.tags
+
+# }
+
+resource "aws_instance" "main" {
   ami = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.sg_ids
-  tags = var.tags
-
+  tags = {
+    Name = "tf-tags"
+  }
 }
--- outgive.tf
+++ outgive.tf
@@ -7,3 +7,10 @@
 #practice 24/06/26
 
 
+output "pub_it" {
+  value = aws_instance.main.public_ip
+}
+
+output "pvt_it" {
+  value = aws_instance.main.private_ip
+}
